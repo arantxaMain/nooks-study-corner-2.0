fc647ec4ff57a8d70d6c7d201c6ccbe170a0a2c5
arantxa <149501497+arantxaMain@users.noreply.github.com>
2025-04-17 16:31:25 +0200

    música hora a hora

--- frontend/src/components/BackgroundMusic.tsx
+++ frontend/src/components/BackgroundMusic.tsx
@@ -1,19 +1,28 @@
 // components/BackgroundMusic.tsx
-import { useEffect, useRef } from 'react';
-import music from '../assets/02. 1200 AM (Sunny).mp3'
+import { useEffect, useState, useRef } from 'react';
+import { playlist } from '../lib/playlist';
 
 export default function BackgroundMusic() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [currentHour, setCurrentHour] = useState(new Date().getHours());  
+  const [music, setMusic] = useState(playlist[currentHour] || playlist[0]);
 
-  const hour = new Date().getHours();
-  setInterval(() => {
-    console.log(hour);
-  }, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newHour = new Date().getHours();
+      if (newHour !== currentHour) {
+        setCurrentHour(newHour);
+        setMusic(playlist[newHour] || playlist[0]);
+      }
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [currentHour]);
 
   useEffect(() => {
     const audio = audioRef.current;
+    console.log(`reproduciendo música de las ${currentHour}`);
     if (audio) {
-      audio.volume = 0.3;
+      audio.volume = 1;
       audio.play().catch(() => {
         console.log('El navegador bloqueó la reproducción automática');
       });
@@ -21,7 +30,7 @@ export default function BackgroundMusic() {
   }, []);
 
   return (
-    <audio ref={audioRef} loop>
+    <audio ref={audioRef} controls loop>
       <source src={music} type="audio/mpeg" />
       Tu navegador no soporta el elemento audio.
     </audio>
